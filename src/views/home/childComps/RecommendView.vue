afa156505588afa824339929c7537095118831ff
sylvia <sylvia174@163.com>
2020-08-01 16:51:16 +0800

    8/1

---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="recommend">
+      <div v-for="(item,index) in recommends" :key="index" class="recommendItem">
+        <a :href="item.link">
+          <img :src="item.image" alt="">
+          <div>{{ item.title }}</div>
+        </a>
+      </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "RecommendView",
+    props: {
+      recommends: {
+        type: Array,
+        default() {
+          return [];
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .recommend {
+    display: flex;
+    width: 100%;
+    text-align: center;
+    font-size: 12px;
+    padding: 15px 0 30px;
+    border-bottom: 10px solid #eee;
+  }
+  .recommendItem {
+    flex: 1;
+  }
+  .recommendItem img {
+    width: 90%;
+    margin-bottom: 10px;
+  }
+</style>
